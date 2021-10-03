ad049cbfebb37571d5d9c05410ff9c02f42bbdaf
Daniel Parkhanovich <daniel.parkhanovich@gmail.com>
2021-10-03 17:23:50 +0200

    Added interact with objects system.

--- Assets/Scripts/Environment/InteractableObject.cs
+++ Assets/Scripts/Environment/InteractableObject.cs
@@ -1,8 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class InteractableObject : EnvironmentObject
 {
-    
+    [SerializeField]
+    private float interactDistance;
+    [SerializeField]
+    private KeyCode keyToInteract = KeyCode.E;
+    [SerializeField]
+    private UnityEvent OnInteractRange;
+    [SerializeField]
+    private UnityEvent ExitInteractRange;
+    [SerializeField]
+    private UnityEvent OnInteractEvent;
+
+    private bool isInteracted = false;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Awake();
+
+        // Check interact, show text
+        if (!isInteracted && SCP.DistanceToTarget < interactDistance)
+        {
+            OnInteractRange.Invoke();
+
+            if (Input.GetKey(keyToInteract))
+            {
+                isInteracted = true;
+                OnInteractEvent.Invoke();
+            }
+        }
+        else
+        {
+            ExitInteractRange.Invoke();
+        }
+
+    }
+
+    public void PrintInteract()
+    {
+        Debug.Log("Interacted");
+    }
 }
